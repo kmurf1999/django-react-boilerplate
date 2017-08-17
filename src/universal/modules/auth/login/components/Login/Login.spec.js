0b3b3136bd937028d086cded8b2e0072381b5b43
ruffers9 <ruffers39@gmail.com>
2017-08-17 16:23:11 -0400

    i forget

--- src/universal/modules/auth/login/components/Login/Login.spec.js
+++ src/universal/modules/auth/login/components/Login/Login.spec.js
@@ -1,1 +1,23 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import TextField from 'material-ui/TextField';
+
+
+import Login from './Login';
+
+describe('Login (Component):', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Login />);
+  });
+
+  it('should render correctly', () => {
+    return expect(wrapper).to.be.ok;
+  });
+
+
+});
